fdb6206104fad1a7be1dd032f2186dd706229e23
jongwoo1126 <lucky4527@gmail.com>
2022-06-09 16:25:42 +0900

    2022/06/09

---
+++ BaekJoon/9단계_기본수학2/1. 소수찾기.py
@@ -0,0 +1,16 @@
+a = int(input())
+list = list(map(int, input().split()))
+count = 0
+
+for i in list:
+    for j in range(2, i+1):
+        if i % j == 0:
+            if i == j:
+                count += 1
+
+            break
+
+print(count)
+
+
+
